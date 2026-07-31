0171bfbedebdd57ede359a26fcff138b9b8e88a4
BryanTaco <bstaco@puce.edu.ec>
2026-07-30 21:28:53 -0500

    Initial Commit

---
+++ PucePark/PuceParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuceParkApp.swift
+//  PucePark
+//
+//  Created by Bryan Taco on 7/30/26.
+//
+
+import SwiftUI
+
+@main
+struct PuceParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
